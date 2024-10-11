f444997f45969856128154a91d7899fb73f5008d
Peter Buzanits <p.buzanits@epb.at>
2024-10-11 19:40:09 +0200

    backported changes from latest projects

--- src/init.php
+++ src/init.php
@@ -9,11 +9,15 @@ trait webapp
   {
     #$libpath = 'vendor/npm-asset/flot';
     $libpath = 'vendor/almasaeed2010/adminlte/plugins/flot';
+    $chartlibpath = 'vendor/almasaeed2010/adminlte/plugins/chart.js';
 
-    if($this->moduleinfo['graph1']):
+    if($this->moduleinfo['graph2']):
       $this->add_includes("<script type='text/javascript' src='{$this->base_dir}{$libpath}/jquery.flot.js'></script>
             <script type='text/javascript' src='{$this->base_dir}{$libpath}/plugins/jquery.flot.time.js'></script>
-            <link rel='stylesheet' type='text/css' href='{$this->base_dir}vendor/booosta/graph1/src/jquery.flot.css' media='screen' />");
+            <script type='text/javascript' src='{$this->base_dir}{$libpath}/plugins/jquery.flot.resize.js'></script>
+            <script type='text/javascript' src='{$this->base_dir}{$libpath}/plugins/jquery.flot.pie.js'></script>
+            <script type='text/javascript' src='{$this->base_dir}{$chartlibpath}/Chart.min.js'></script>
+            <link rel='stylesheet' type='text/css' href='{$this->base_dir}vendor/booosta/graph2/src/jquery.flot.css' media='screen' />");
     endif;
   }
 }
